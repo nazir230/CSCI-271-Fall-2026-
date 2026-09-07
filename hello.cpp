7884b311563f93ace1b8f2a4df17ad1d37d9c28a
nazir230 <nazir.hossain@jjay.cuny.edu>
2026-09-07 16:06:27 -0400

    Add hello.cpp for greeting user

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+// Naz Hossain
+// CSCI 271 - Assignment 1
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, world!" << endl;
+
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+
+    cout << "Nice to meet you, " << name << "!";
+
+    return 0;
+}
