ce508ec56611086b9af3e81848cdfa5e5d2a1fa4
nazir230 <nazir.hossain@jjay.cuny.edu>
2026-09-22 15:41:34 -0400

    Add temperature classification based on input

--- temp_check.cpp
+++ temp_check.cpp
@@ -1,1 +1,27 @@
+// Name: Nazir Hossain
+// Assignment 3
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temperature;
+
+    cout << "Enter the temperature (F): ";
+    cin >> temperature;
+
+    if (temperature < 32) {
+        cout << "That's Freezing." << endl;
+    }
+    else if (temperature <= 59) {
+        cout << "That's Cold." << endl;
+    }
+    else if (temperature <= 75) {
+        cout << "That's Mild." << endl;
+    }
+    else {
+        cout << "That's Hot." << endl;
+    }
+
+    return 0;
+}
